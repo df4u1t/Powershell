a547ac15da73193d5d3be082dd3c76381bf4d089
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2019-10-08 17:01:31 -0500

    inactive ad account script

---
+++ inactive-adaccounts.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module ActiveDirectory
+# No of days - Get AD users who are no logged in last 90 days
+$DaysInactive = 180  
+$time = (Get-Date).Adddays(-($DaysInactive)) 
+Get-ADUser -Filter { LastLogonTimeStamp -lt $time -and enabled -eq $true -and userPrincipalName -ne '*' } -Properties * | 
+ 
+Select-Object Name,UserPrincipalName, @{Name="LastLogonTimeStamp"; 
+Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp).ToString()}} |
+# Export Inactive Users Report to CSV file 
+Export-CSV "C:\InactiveADUsers.csv" -NoTypeInformation -Encoding UTF8
