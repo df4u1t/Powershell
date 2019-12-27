f6e327ad7e0f424fb0f7ce60bf6553c2fc938be8
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2019-12-27 16:46:27 -0600

    added reg values for guest auth to smb servers

--- BenchBuild-113018.ps1
+++ BenchBuild-113018.ps1
@@ -1,17 +1,18 @@
-﻿If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
   # Relaunch as an elevated process:
   Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
   exit
 }
 
-$WorkDir = "C:\installer"
+$WorkDir = "C:\usr"
 $AdobeUrl = 'https://admdownload.adobe.com/bin/live/readerdc_en_xa_crd_install.exe'
 $ChromeUrl = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
 $ODTUrl = "\\agj-bnap\ODT"
 $Creds = Get-Credential
 
 mkdir $WorkDir
+Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters -Name AllowInsecureGuestAuth -Value 1
 
 #build file name
 $AdobeSetup = "readerdc_en_xa_crd_install.exe"
@@ -19,6 +20,7 @@ $ChromeSetup = "chrome_installer.exe"
 $ODTSetup = "setup.exe"
 $ODTConfig = $ODTUrl + "\install.xml"
 $ODTArgs = "/configure " + $ODTConfig
+$ODTPath = Join-Path -Path $ODTUrl -ChildPath $ODTSetup
 
 #download file
 Invoke-WebRequest -Uri $AdobeUrl -OutFile $WorkDir\$AdobeSetup
@@ -29,7 +31,7 @@ Start-Process -FilePath "$WorkDir\$AdobeSetup"
 Wait-Process -Name "reader*"
 Start-Process -FilePath "$WorkDir\$ChromeSetup"
 Wait-Process -Name "chrome*"
-Start-Process -Credential $Creds -WorkingDirectory "$WorkDir" -FilePath "$ODTUrl\$ODTSetup" -ArgumentList "$ODTArgs"
+Start-Process -WorkingDirectory "$WorkDir" -FilePath "$ODTPath" -ArgumentList "$ODTArgs"
 Wait-Process -Name "setup.exe"
 
 #disables power management on all NICs and sets power options to never sleep on battery and while plugged in
@@ -39,6 +41,7 @@ powercfg /change standby-timeout-dc 0
 powercfg /change hibernate-timeout-ac 0
 powercfg /change hibernate-timeout-dc 0
 
-#$PCName = Read-Host "Enter new PC name: "
-#Rename-Computer -NewName $PCName
-
+$PCName = Read-Host "Enter new PC name: "
+Rename-Computer -NewName $PCName
+sleep 5
+shutdown /r /t 0
