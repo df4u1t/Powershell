cd89e797118e4cf08bb95721a559d13075245eb0
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2020-01-15 09:48:42 -0600

    Added specific AppData directories to include and import/export ProfileList from registry

--- datatrans.ps1
+++ datatrans.ps1
@@ -8,8 +8,19 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 #Specifies Source, Destination, and Working Directory Paths
 $source = Read-Host "Please enter the source directory for the Users data: (ex. d:\users\jdoe)"
 $destination = Read-Host "Please enter the destination directory to store the Users data: (ex. c:\usr\jdoe)"
+$user = Read-Host "Please enter the username you're copying data from: (ex. jdoe)"
+$drive = Read-Host "Please enter the drive letter the source OS is installed on: (ex. D:\)"
 $pwd = 'c:\temp'
+#$regHKLM = 'HKLM\TempHive'
 mkdir $pwd
 
 #Starts Robocopy to transfer all attributes, copies timestamps to destination, and outputs a log to the working directory.
 robocopy $source $destination /XD 'AppData' 'Application Data' /E /ZB /DCOPY:T /COPYALL /MT:128 /R:0 /W:0 /V /TEE /LOG:$pwd\Robocopy.log
+robocopy $source $destination + '\AppData\Local\Google' /E /ZB /DCOPY:T /COPYALL /MT:128 /R:0 /W:0 /V /TEE /LOG:$pwd\Robocopy.log
+robocopy $source $destination + '\AppData\Local\Microsoft\Office' /E /ZB /DCOPY:T /COPYALL /MT:128 /R:0 /W:0 /V /TEE /LOG:$pwd\Robocopy.log
+robocopy $source $destination + '\AppData\Local\Microsoft\Outlook' /E /ZB /DCOPY:T /COPYALL /MT:128 /R:0 /W:0 /V /TEE /LOG:$pwd\Robocopy.log
+robocopy $source $destination + '\AppData\Roaming\Microsoft\Windows\Themes' /E /ZB /DCOPY:T /COPYALL /MT:128 /R:0 /W:0 /V /TEE /LOG:$pwd\Robocopy.log
+$SID = Get-WmiObject -Class win32_userAccount -Filter "name = '$user'" | foreach { $_.SID }
+#reg load $regHKLM $drive + 'Windows\System32\config\SOFTWARE'
+reg export $regHKLM + '\Microsoft\Windows NT\CurrentVersion\ProfileList\' + $SID $destination + '\user.reg'
+#reg import $destination + '\user.reg'
