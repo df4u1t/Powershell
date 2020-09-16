d68a4b876ba9a5e995bbba19dd19c01a2de2f08b
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2020-09-16 15:42:01 -0500

    Updated

--- admin-pwreset.ps1
+++ admin-pwreset.ps1
@@ -1,14 +1,22 @@
 $groups = 'Administrators', 'Enterprise Admins', 'Schema Admins', 'SMSMSE Admins', 'Domain Admins'
-$users = Get-ADGroupMember -Identity $group -Recursive | Sort-Object -Unique | Select -ExpandProperty SamAccountName
 
 function Get-RandomCharacters($length, $characters) { 
     $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length } 
     $private:ofs="" 
     return [String]$characters[$random]
 }
-
+foreach ($group in $groups) {
+    $users = Get-ADGroupMember -Identity $group -Recursive | Sort-Object -Unique | Select -ExpandProperty SamAccountName
+}
 foreach ($user in $users) {
-    $password = Get-RandomCharacters -length 16 -characters 'abcdefghiklmnoprstuvwxyzABCDEFGHKLMNOPRSTUVWXYZ1234567890!"§$%&/()=?}][{@#*+'
-    Add-Content -Path C:\TempPath\creds.txt -value "$user new password is $password"
-    Set-ADAccountPassword -Identity $users -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)
+    if ($user -eq 'CHANGEME') {
+        $password = 'CHANGEME'
+        Add-Content -Path C:\TempPath\creds.txt -value "$user new password is $password"
+        Set-ADAccountPassword -Identity $users -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)
+    }
+    else {
+        $password = Get-RandomCharacters -length 16 -characters 'abcdefghiklmnoprstuvwxyzABCDEFGHKLMNOPRSTUVWXYZ1234567890!"§$%&/()=?}][{@#*+'
+        Add-Content -Path C:\TempPath\creds.txt -Value "$user new password is $password"
+        Set-ADAccountPassword -Identity 'agjadmin' -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)
+    }
 }
