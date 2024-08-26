194daed1ffffc6698dda23046504900c64a994af
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2024-08-26 11:27:53 -0500

    Update Block-M365items.ps1

--- Block-M365items.ps1
+++ Block-M365items.ps1
@@ -1,18 +1,22 @@
 Set-ExecutionPolicy RemoteSigned
 
-$senderdomain = ""
-$url = ""
-$hash = ""
-
 ##Must have ExchangeOnlineManagement module already installed
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline -ShowProgress $true
+
+##Sets items to be blocked.
+$senderdomain = ""
+$url = ""
+$hash = ""
 
 ##Creates new entry for a blocked sender or domain
 New-TenantAllowBlockListItems -ListType Sender -Entries "$senderdomain" -Block -NoExpiration
 
 ##Creates new entry for a blocked URL
-New-TenantAllowBlockListItems -ListType Url -Entries "$url" -Block -NoExpiration
+New-TenantAllowBlockListItems -ListType URL -Entries "$url" -Block -NoExpiration
 
 ##Creates new entry for a blocked file hash
 New-TenantAllowBlockListItems -ListType FileHash -Entries "$hash" -Block -NoExpiration
+
+##Disconnect from powershell session
+Disconnect-ExchangeOnline
