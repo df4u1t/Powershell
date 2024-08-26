b5bc66331e06904daf7e7925b1399e46028c5b3b
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2024-08-26 10:50:28 -0500

    Create Block-M365items.ps1

---
+++ Block-M365items.ps1
@@ -0,0 +1,18 @@
+Set-ExecutionPolicy RemoteSigned
+
+$sender = ""
+$url = ""
+$hash = ""
+
+##Must have ExchangeOnlineManagement module already installed
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline -ShowProgress $true
+
+##Creates new entry for a blocked sender or domain
+New-TenantAllowBlockListItems -ListType Sender -Entries "$sender" -Block -NoExpiration
+
+##Creates new entry for a blocked URL
+New-TenantAllowBlockListItems -ListType URL -Entries "$url" -Block -NoExpiration
+
+##Creates new entry for a blocked file hash
+New-TenantAllowBlockListItems -ListType FileHash -Entries "$hash" -Block -NoExpiration
