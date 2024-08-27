3ee887caadb04cf2ca6072b8107b1f8e465a8d86
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2024-08-27 10:55:46 -0500

    Delete Delete-NetResponse.ps1

--- Delete-NetResponse.ps1
+++
@@ -1,3 +0,0 @@
-## Disable Active Response Windows Firewall Rule
-Set-NetFirewallRule -DisplayName "@@Inbound_ActiveResponse" -Enabled False
-Set-NetFirewallRule -DisplayName "@@Outbound_ActiveResponse" -Enabled False
