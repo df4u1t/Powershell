ac2b08df8829cf92bff4ac19f7d53207e2e7262f
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2024-08-27 09:17:02 -0500

    Update Block-M365items.ps1

--- Block-M365items.ps1
+++ Block-M365items.ps1
@@ -1,22 +1,34 @@
+param (
+	[switch] $MaliciousDomain,
+	[switch] $MaliciousURL,
+	[switch] $MaliciousHash
+)
+
 Set-ExecutionPolicy RemoteSigned
 
 ##Must have ExchangeOnlineManagement module already installed
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline -ShowProgress $true
 
-##Sets items to be blocked.
+##Sets items to be blocked. If more than one start each successive entry with a ,
 $senderdomain = ""
 $url = ""
 $hash = ""
 
+if ($MaliciousDomain) {
 ##Creates new entry for a blocked sender or domain
-New-TenantAllowBlockListItems -ListType Sender -Entries "$senderdomain" -Block -NoExpiration
+	New-TenantAllowBlockListItems -ListType Sender -Entries "$senderdomain" -Block -NoExpiration
+}
 
+if ($MaliciousURL) {
 ##Creates new entry for a blocked URL
-New-TenantAllowBlockListItems -ListType URL -Entries "$url" -Block -NoExpiration
+	New-TenantAllowBlockListItems -ListType Url -Entries "$url" -Block -NoExpiration
+}
 
+if ($MaliciousHash) {
 ##Creates new entry for a blocked file hash
-New-TenantAllowBlockListItems -ListType FileHash -Entries "$hash" -Block -NoExpiration
+	New-TenantAllowBlockListItems -ListType FileHash -Entries "$hash" -Block -NoExpiration
+}
 
 ##Disconnect from powershell session
 Disconnect-ExchangeOnline
