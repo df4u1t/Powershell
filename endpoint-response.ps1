c510a46263f2ae1048a92691b29384efdb13d50e
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2024-08-27 10:49:27 -0500

    Update endpoint-response.ps1

--- endpoint-response.ps1
+++ endpoint-response.ps1
@@ -19,34 +19,41 @@ if ($MaliciousRegKey) {
 ## Adds entry to Windows Firewall rule
 if ($MaliciousIP) {
 	$ips = (Get-NetFirewallRule -DisplayName "@@_Inbound_ActiveResponse" | Get-NetFirewallAddressFilter).remoteaddress
-	$new_ips = @(Read-Host "Please enter the malicious IP you want to block.")
+	$new_ips = @(Read-Host "Enter the malicious IP you want to block.")
 	$merge = $ips + $new_ips
 	$merge = $merge | select -Unique | sort
 	Set-NetFirewallRule -DisplayName "@@Inbound_ActiveResponse" -RemoteAddress "$merge"
 	Set-NetFirewallRule -DisplayName "@@Outbound_ActiveResponse" -RemoteAddress "$merge"
 }
 
+## Forcefully deletes path and all recursive items.
 if ($MaliciousItem) {
-	
+	$path = Read-Host "Enter the path or item you wish to delete."
+	Remove-Item -recurse -force $path
 }
 
+## Deletes a malicious Scheduled Task.
 if ($MaliciousTask) {
-	
+	$malicious_task = Read-Host "Enter the malicious task name you wish to delete."
+	Unregister-ScheduledTask -TaskName $malicious_task -Confirm:$false
 }
 
+## Stops and deletes a malicious service.
 if ($MaliciousService) {
-	
+	$malicious_service = Read-Host "Enter the name of the malicious service you wish to delete."
+	Stop-Service -Name "$malicious_service"
+	Remove-Service -Name "$malicious_service"
 }
 
 ## Creates a null route for the specified IP.
 if ($AddNullRoute) {
-	$nullroute = Read-Host "Please enter the IP you want to create a null route for."
+	$nullroute = Read-Host "Enter the IP you want to create a null route for."
 	route add $nullroute 255.255.255.255 169.169.169.169 if 1
 }
 
 ## Creates ingress and egress Windows Firewall rules, use this option if the Firewall rules do not exist.
 if ($AddFirewallRule) {
-	$malicious_IP = Read-Host "Please enter the malicious IP you wish to block."
+	$malicious_IP = Read-Host "Enter the malicious IP you wish to block."
 	New-NetFirewallRule -DisplayName "@@_Inbound_ActiveResponse" -Direction Inbound -Action Block -RemoteAddress "$malicious_IP"
 	New-NetFirewallRule -DisplayName "@@_Outbound_ActiveResponse" -Direction Outbound -Action Block -RemoteAddress "$malicious_IP"
 }
