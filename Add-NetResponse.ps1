1a20975129689852ee89d254374c5e38ef0cb383
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2024-08-27 10:43:38 -0500

    Delete Add-NetResponse.ps1

--- Add-NetResponse.ps1
+++
@@ -1,7 +0,0 @@
-## Add entry to Windows Firewall rule
-$ips = (Get-NetFirewallRule -DisplayName "@@_Inbound_ActiveResponse" | Get-NetFirewallAddressFilter).remoteaddress
-$new_ips = @("change_me")
-$merge = $ips + $new_ips
-$merge = $merge | select -Unique | sort
-Set-NetFirewallRule -DisplayName "@@Inbound_ActiveResponse" -RemoteAddress "$merge"
-Set-NetFirewallRule -DisplayName "@@Outbound_ActiveResponse" -RemoteAddress "$merge"
