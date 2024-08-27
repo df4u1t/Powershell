ed217078f36961449e83dcf6ad4db4b8b2440b41
DF4U1T <50423443+df4u1t@users.noreply.github.com>
2024-08-27 10:01:42 -0500

    Update m365-response.ps1
    script was failing due to placement of variable and qutations.

--- m365-response.ps1
+++ m365-response.ps1
@@ -12,20 +12,20 @@ Connect-ExchangeOnline -ShowProgress $true
 
 if ($MaliciousDomain) {
 ##Creates new entry for a blocked sender or domain
-	$senderdomain = Read-Host "Please enter the malicious sender or domain with quotations. You may enter more that one entry using a ','"
-	New-TenantAllowBlockListItems -ListType Sender -Entries"$senderdomain" -Block -NoExpiration
+	$senderdomain = Read-Host "Please enter the malicious sender or domain."
+	New-TenantAllowBlockListItems -ListType Sender -Entries $senderdomain -Block -NoExpiration
 }
 
 if ($MaliciousURL) {
 ##Creates new entry for a blocked URL
-	$url = Read-Host "Please enter the malicious url with quotations. You may enter more that one entry using a ','"
-	New-TenantAllowBlockListItems -ListType Url -Entries"$url" -Block -NoExpiration
+	$url = Read-Host "Please enter the malicious url."
+	New-TenantAllowBlockListItems -ListType Url -Entries $url -Block -NoExpiration
 }
 
 if ($MaliciousHash) {
-	$hash = Read-Host "Please enter the malicious file hash with quotations. You may enter more that one entry using a ','"
+	$hash = Read-Host "Please enter the malicious file hash."
 ##Creates new entry for a blocked file hash
-	New-TenantAllowBlockListItems -ListType FileHash -Entries"$hash" -Block -NoExpiration
+	New-TenantAllowBlockListItems -ListType FileHash -Entries $hash -Block -NoExpiration
 }
 
 ##Disconnect from powershell session
